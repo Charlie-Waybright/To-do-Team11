c7bdc0c27637be4bccc95c806d2ca8257ec8d149
Charlie-Waybright <charliegh.waybright@ascte.org>
2025-08-29 11:26:58 -0500

    Update To.py

--- To.py
+++ To.py
@@ -2,6 +2,7 @@
 
 # Step 1: Start with an empty list to hold tasks
 tasks = []
+Done = []
 
 # Step 2: Add a task
 def add_task(task):
@@ -20,6 +21,13 @@ def delete_task(index):
 
 # Step 5: Mark task complete
 
+def mark_complete():
+    tasks.remove(index)
+    Done.append(index)
+    print(f"Finished: {tasks}")
+    print(Done)
+
+
 
 
 # Demo flow (you can run this file directly: python todo.py)
@@ -28,5 +36,5 @@ if __name__ == "__main__":
     add_task("Push code to GitHub")
     delete_task(0)
     view_tasks()
-    #mark_complete(0)
+    mark_complete()
     #view_tasks()
